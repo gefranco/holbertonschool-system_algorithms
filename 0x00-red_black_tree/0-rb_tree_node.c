91fce6be7a30f6c3d345e1002928d5366242455a
gefranco <933@holbertonschool.com>
2021-03-02 00:18:43 +0000

    [fix] betty

--- 0x00-red_black_tree/0-rb_tree_node.c
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -3,31 +3,29 @@
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
 
-	rb_tree_t *node;	
+	rb_tree_t *node;
 
 	if (!parent)
 	{
 		parent = malloc(sizeof(rb_tree_t));
 		if (parent == NULL)
-			return NULL;
+			return (NULL);
 		parent->parent = NULL;
 		parent->color = color;
 		parent->n = value;
 		parent->left = NULL;
 		parent->right = NULL;
-		return parent;
+		return (parent);
 	}
 
 	node = malloc(sizeof(rb_tree_t));
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 	node->parent = parent;
 	node->n = value;
 	node->color = color;
 	node->left = NULL;
 	node->right = NULL;
 
-
-
-	return node;
+	return (node);
 }
