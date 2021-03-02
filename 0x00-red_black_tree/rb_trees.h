479ad1c670add9a78b61ea33926802ebfcff5515
gefranco <933@holbertonschool.com>
2021-03-02 00:27:36 +0000

    [add] doc

--- 0x00-red_black_tree/rb_trees.h
+++ 0x00-red_black_tree/rb_trees.h
@@ -1,13 +1,27 @@
 #ifndef RB_TREES
 #define RB_TREES
-
+/**
+ * enum rb_color_e - Possible color of a Red-Black tree
+ *
+ * @RED: 0 -> Red node
+ * @BLACK: 1 -> Black node
+ * @DOUBLE_BLACK: ?
+ */
 typedef enum rb_color_e
 {
 	RED = 0,
 	BLACK,
 	DOUBLE_BLACK
 } rb_color_t;
-
+/**
+ * struct rb_tree_s - Red-Black tree node structure
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @color: Color of the node (RED or BLACK)
+ */
 typedef struct rb_tree_s
 {
 	int n;
