f703662ef7a695e9b0a2564f64520f0b4e74033a
gefranco <933@holbertonschool.com>
2021-05-03 16:38:35 +0000

    [add] function that creates a symbol_t data structure

---
+++ 0x02-huffman_coding/symbol.c
@@ -0,0 +1,12 @@
+#include "huffman.h"
+symbol_t *symbol_create(char data, size_t freq)
+{
+	symbol_t *symbol;
+
+	symbol = malloc(sizeof(symbol_t));
+	if (!symbol)
+		return (NULL);
+	symbol->data = data;
+	symbol->freq = freq;
+	return (symbol);
+}
