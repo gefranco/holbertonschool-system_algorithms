1ab7cb21fce946ff286c2b77fb7fc523cc0fe160
gefranco <933@holbertonschool.com>
2021-05-07 02:22:21 +0000

    [add]  a function that builds the Huffman tree

---
+++ 0x02-huffman_coding/huffman_tree.c
@@ -0,0 +1,18 @@
+#include "heap.h"
+#include "huffman.h"
+
+/**
+ * huffman_tree - Entry point
+ * @data: an array of characters
+ * @freq: an array containing the associated frequencies
+ * @size: the arrays size
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
+{
+	(void) data;
+	(void) freq;
+	(void) size;
+	return (NULL);
+}
+
