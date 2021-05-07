304add814cc031aceaabf9927384aded59e37e29
gefranco <933@holbertonschool.com>
2021-05-07 02:28:08 +0000

    [add]  function that build the Huffman tree and print the resulting Huffman codes for each symbol

---
+++ 0x02-huffman_coding/huffman_codes.c
@@ -0,0 +1,17 @@
+#include "huffman.h"
+#include "heap.h"
+
+/**
+ * huffman_codes - Entry point
+ * @data:  an array of characters
+ * @freq: an array containing the associated frequencies
+ * @size: size of the arrays
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int huffman_codes(char *data, size_t *freq, size_t size)
+{
+	(void) data;
+	(void) freq;
+	(void) size;
+	return (1);
+}
