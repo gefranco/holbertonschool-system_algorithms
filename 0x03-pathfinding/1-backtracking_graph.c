d2741add546c03f96013b7c771efa5b4a606776a
gefranco <933@holbertonschool.com>
2021-05-28 03:05:34 +0000

    [fix] betty

--- 0x03-pathfinding/1-backtracking_graph.c
+++ 0x03-pathfinding/1-backtracking_graph.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-queue_t *backtracking(queue_t *path, vertex_t const *start, const vertex_t *target, char *);
-queue_t *backtracking_graph(graph_t *graph, vertex_t const *start, vertex_t const *target)
+queue_t *backtracking(queue_t *path, vertex_t const *start,
+			const vertex_t *target, char *);
+queue_t *backtracking_graph(graph_t *graph,
+				vertex_t const *start, vertex_t const *target)
 {
 	char *visited;
 	queue_t *path = queue_create();
@@ -11,37 +13,37 @@ queue_t *backtracking_graph(graph_t *graph, vertex_t const *start, vertex_t cons
 
 	visited = calloc(graph->nb_vertices, sizeof(char));
 	path = backtracking(path, start, target, visited);
-	queue_push_front(path, strdup(start->content));		
+	queue_push_front(path, strdup(start->content));
 	free(visited);
-	return path;	
-	
+	return (path);
 
 }
 
-queue_t *backtracking(queue_t *path, vertex_t const *start, const vertex_t *target, char *visited)
+queue_t *backtracking(queue_t *path, vertex_t const *start,
+			const vertex_t *target, char *visited)
 {
 	edge_t *edges;
 	(void) path;
 	(void) start;
 	(void) target;
-		
+
 	if (!start || visited[start->index])
-		return NULL;
-		
+		return (NULL);
+
 	visited[start->index] = 1;
 	printf("Checking %s\n", start->content);
 
-	if(start == target)
+	if (start == target)
 	{
-		return(path);
-	}	
+		return (path);
+	}
 	for (edges = start->edges; edges; edges = edges->next)
 	{
-		if(backtracking(path, edges->dest, target, visited))
+		if (backtracking(path, edges->dest, target, visited))
 		{
-			queue_push_front(path, strdup(edges->dest->content));	
-			return path;
+			queue_push_front(path, strdup(edges->dest->content));
+			return (path);
 		}
 	}
-	return (NULL);	
+	return (NULL);
 }
