537322ed6be2eaf1aec4558cd2f5be55f80ba456
gefranco <933@holbertonschool.com>
2021-07-13 22:03:08 +0000

    [fix]betty style

--- 0x04-nary_trees/0-nary_tree_insert.c
+++ 0x04-nary_trees/0-nary_tree_insert.c
@@ -1,12 +1,20 @@
 #include <stdlib.h>
 #include "nary_trees.h"
 #include <string.h>
+
+/**
+ * nary_tree_insert - function that inserts a node in a N-ary tree
+ * @parent: a pointer to the parent node
+ * @str: the string to be stored in the created node
+ * Return: a pointer to the created node, or NULL on failure
+ */
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 {
 	nary_tree_t *node, *tmp;
+
 	node = malloc(sizeof(nary_tree_t));
 	node->content = strdup(str);
-	
+
 	if (!parent)
 	{
 		parent = node;
@@ -14,17 +22,17 @@ nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 		parent->nb_children = 0;
 		parent->children = NULL;
 		parent->next = NULL;
-		return (parent);		
-	}	
-	
+		return (parent);
+	}
+
 	node->parent = parent;
 	node->nb_children = 0;
 	node->children = NULL;
 	tmp = parent->children;
 
 	parent->children = node;
-	node->next = tmp;	
+	node->next = tmp;
 	parent->nb_children += 1;
-			
+
 	return (node);
 }
